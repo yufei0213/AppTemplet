6d62b674f778f4e1dfc4a84beb87473853bcb1ef
王玉飞 <wangyufe0213@gmail.com>
2016-10-08 16:46:04 +0800

    no message

--- app/src/main/java/com/yufei/apptemplet/utils/AppUtil.java
+++ app/src/main/java/com/yufei/apptemplet/utils/AppUtil.java
@@ -193,6 +193,107 @@ public class AppUtil {
     }
 
     /**
+     * get macAddress
+     *
+     * @return
+     */
+    public static String getMacAddress() {
+
+        String mac = null;
+        FileReader fstream = null;
+
+        try {
+
+            try {
+
+                fstream = new FileReader("/sys/class/net/wlan0/address");
+            } catch (FileNotFoundException e) {
+
+                fstream = new FileReader("/sys/class/net/eth0/address");
+            }
+
+            BufferedReader in = null;
+
+            if (fstream != null) {
+
+                try {
+
+                    in = new BufferedReader(fstream, 1024);
+                    mac = in.readLine();
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+                } finally {
+
+                    if (fstream != null) {
+
+                        try {
+
+                            fstream.close();
+                        } catch (IOException e) {
+
+                            e.printStackTrace();
+                        }
+                    }
+
+                    if (in != null) {
+
+                        try {
+
+                            in.close();
+                        } catch (IOException e) {
+
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+
+            return mac;
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * get device_id
+     *
+     * @param context
+     * @return
+     */
+    public static String getDeviceId(Context context) {
+
+        try {
+
+            org.json.JSONObject json = new org.json.JSONObject();
+            android.telephony.TelephonyManager tm = (android.telephony.TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+            String device_id = null;
+
+            if (checkPermission(context, Manifest.permission.READ_PHONE_STATE))
+                device_id = tm.getDeviceId();
+
+            String mac = AppUtil.getMacAddress();
+
+            if (TextUtils.isEmpty(device_id))
+                device_id = mac;
+
+            if (TextUtils.isEmpty(device_id))
+                device_id = android.provider.Settings.Secure.getString(context.getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+
+            return device_id;
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
      * get device_id and mac
      *
      * @param context
